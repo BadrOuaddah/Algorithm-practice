80e419546e2c7e56a9457a2dac0476e943224568
Admin <Badr.Ouaddah.07@gmail.com>
2023-05-24 12:54:12 +0100

    Create Gradle algorithm java project

--- src/main/java/com/badr/algorithm/Main.java
+++ src/main/java/com/badr/algorithm/Main.java
@@ -1,4 +1,6 @@
-package com.badr.algorithm;public class Main {
+package com.badr.algorithm;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
