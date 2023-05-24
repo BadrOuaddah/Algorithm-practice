c69e01382b0fc0ac943d4dbb3be19aa1da0b3574
Admin <Badr.Ouaddah.07@gmail.com>
2023-05-24 13:55:04 +0100

    Solved four java challenges HackerRank

---
+++ src/main/java/com/badr/algorithm/javaHelloWorld.java
@@ -0,0 +1,8 @@
+package com.badr.algorithm;
+public class javaHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
