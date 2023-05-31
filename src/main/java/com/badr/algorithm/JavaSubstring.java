4be06e31e2a95eeba02fdaac980ef8dcb3d0b4a3
Admin <Badr.Ouaddah.07@gmail.com>
2023-05-31 18:53:12 +0100

    Solve java challenges HackerRank (Java Substring)

---
+++ src/main/java/com/badr/algorithm/JavaSubstring.java
@@ -0,0 +1,12 @@
+package com.badr.algorithm;
+import java.util.*;
+public class JavaSubstring {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String s = S.substring(start, end);
+        System.out.println(s);
+    }
+}
