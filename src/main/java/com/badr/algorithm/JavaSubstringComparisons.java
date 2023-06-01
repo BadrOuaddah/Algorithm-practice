6272e140380ce6026bce1d80a3389900f52bd186
Admin <Badr.Ouaddah.07@gmail.com>
2023-06-01 22:00:07 +0100

    Solve java challenges HackerRank (Java Substring Comparisons)

---
+++ src/main/java/com/badr/algorithm/JavaSubstringComparisons.java
@@ -0,0 +1,28 @@
+package com.badr.algorithm;
+import java.util.Scanner;
+
+public class JavaSubstringComparisons {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0,3);
+        String largest = "";
+        for(int i=0;i<s.length()-k+1;i++){
+            String temp=s.substring(i,i+k);
+            if(temp.compareTo(largest) > 0)
+            {
+                largest = temp;
+            }
+            if(temp.compareTo(smallest) < 0){
+                smallest = temp;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
