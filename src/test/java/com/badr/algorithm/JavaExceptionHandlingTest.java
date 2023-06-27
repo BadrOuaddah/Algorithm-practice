73c80b4c415ee1e78dcc86bdac72fcb720d17fe6
Admin <Badr.Ouaddah.07@gmail.com>
2023-06-27 11:34:17 +0100

    Add tests for the happy paths

--- src/test/java/com/badr/algorithm/JavaExceptionHandlingTest.java
+++ src/test/java/com/badr/algorithm/JavaExceptionHandlingTest.java
@@ -1,14 +1,23 @@
 package com.badr.algorithm;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JavaExceptionHandlingTest {
     MyCalculator myCalculator = new MyCalculator();
-    JavaExceptionHandling javaExceptionHandling = new JavaExceptionHandling();
+
+    @Test
+    public void testExceptionWhenNumberIsPositive() throws Exception {
+        myCalculator.power(1,2);
+
+    }
 
     @Test(expected = Exception.class)
-    public void testException() throws Exception {
+    public void testExceptionWhenNumberIsNegative() throws Exception {
         myCalculator.power(-1,2);
     }
     @Test
