958ac9f0ab0422bf4a7b11060bc9f56351acbc80
Admin <Badr.Ouaddah.07@gmail.com>
2023-07-27 12:26:40 +0100

    Refactor code in JavaSingletonPattern file

--- src/main/java/com/badr/algorithm/JavaSingletonPattern.java
+++ src/main/java/com/badr/algorithm/JavaSingletonPattern.java
@@ -1,12 +1,5 @@
 package com.badr.algorithm;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-import java.lang.reflect.*;
-
 class Singleton{
     private Singleton() {
 
